92770a5785424c621b4f2bee475dd0f8d3d1828c
xipinfan <1310721316@qq.com>
2020-04-21 21:50:49 +0800

    添加了主页面，以及基础app

---
+++ student/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Gallery
+
+# Create your views here.
+def home(request):
+    gallery = Gallery.objects.all()
+    return render(request, 'index.html', context={'gallery': gallery})
