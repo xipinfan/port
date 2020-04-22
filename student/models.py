d02096d6a96ba99ba645b04fab45f2a28eafd243
xipinfan <1310721316@qq.com>
2020-04-22 16:19:31 +0800

    添加了图片的展示和上传，修改了一些小问题

--- student/models.py
+++ student/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-
 class Gallery(models.Model):
     description = models.CharField(max_length=100, verbose_name="概述")
+    image = models.ImageField(default='defaule.png', upload_to='mage/')
+    title = models.CharField(max_length=50, default='作品标题')
+    def __str__(self):
+        return self.title
