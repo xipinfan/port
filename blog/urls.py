e5b76ec32af7de403abcf4075ba799a550b40b01
xipinfan <1310721316@qq.com>
2020-04-22 22:11:15 +0800

    新增加了文章详情子页面

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog)
+    path('', views.blog),
+    path('<int:blog_id>/', views.text1),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Blog
 # Create your views here.
 
 def blog(request):
     text = Blog.objects.all()
     return render(request, 'blog.html', context={'text': text})
+
+def text1(request, blog_id):
+    text2 = get_object_or_404(Blog, pk=blog_id)
+    #text2 = Blog.objects.all()[blog_id-1]
+    return render(request, 'blog_text.html',{'text': text2})
